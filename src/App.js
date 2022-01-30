7e0d1b7e7a055595f942705b367b2f2a06b1f534
JacobRyanHanson <jacobryanhanson@gmail.com>
2022-01-30 11:55:19 -0600

    Add contact section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { validateEmail } from './utils/helper';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Project from './components/Project';
@@ -7,13 +8,42 @@ import resume from "./assets/resume/Resume.pdf";
 
 function App() {
 	const [categories] = useState([
-		{ name: 'About Me' },
+		{ name: 'About' },
 		{ name: 'Portfolio' },
 		{ name: 'Contact' },
 		{ name: 'Resume' },
 	]);
 
 	const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+	const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+	const [errorMessage, setErrorMessage] = useState('');
+	const { name, email, message } = formState;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!errorMessage) {
+			setFormState({ [e.target.name]: e.target.value });
+			console.log('Form', formState);
+		}
+	};
+
+	const handleChange = (e) => {
+		if (e.target.name === 'email') {
+			const isValid = validateEmail(e.target.value);
+			if (!isValid) {
+				setErrorMessage('Your email is invalid.');
+			} else {
+				setErrorMessage('');
+			}
+		} else {
+			if (!e.target.value.length) {
+				setErrorMessage(`${e.target.name} is required.`);
+			} else {
+				setErrorMessage('');
+			}
+		}
+	};
 
 	return (
 		<div>
@@ -26,7 +56,7 @@ function App() {
 			<main>
 				{currentCategory === categories[0] &&
 					<>
-						<h1 id="about-me">About me</h1>
+						<h1 id="about-me">About Me</h1>
 						<img src={avatar} id="avatar" alt="avatar" />
 						<p className="about-text">
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id risus in nisl pellentesque
@@ -61,12 +91,32 @@ function App() {
 				}
 
 				{currentCategory === categories[1] &&
-						<Project></Project>
+					<Project></Project>
 				}
 
 				{currentCategory === categories[2] &&
 					<>
-						<h1>Type3: </h1>
+						<h1 id="contact-form-title">Contact Me</h1>
+						<form id="contact-form" onSubmit={handleSubmit}>
+							<div className="name-container">
+								<label htmlFor="name">Name:</label>
+								<input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+							</div>
+							<div className="email-container">
+								<label htmlFor="email">Email Address:</label>
+								<input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+							</div>
+							<div className="message-container">
+								<label htmlFor="message">Message:</label>
+								<textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+							</div>
+							{errorMessage && (
+								<div>
+									<p className="error-text">{errorMessage}</p>
+								</div>
+							)}
+							<button type="submit">Submit</button>
+						</form>
 					</>
 				}
 
